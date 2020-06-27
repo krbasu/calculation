f5ea5912f2dd1280fe45a418d88f8cf3d9f9bc73
krbasu <64273116+krbasu@users.noreply.github.com>
2020-06-27 12:44:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <div>
+            <p>Please Download The Pdf</p>
+            <a href="download.php?target=RIMIPOST" target="_blank">Download Now</a>
+        </div>
+    </center>
+</body>
+</html>
